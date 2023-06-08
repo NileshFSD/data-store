c683204b6ddcfb2240fbd32e479e7fdac045f099
Nilesh_Savaliya <106680901+NileshFSD@users.noreply.github.com>
2023-06-08 15:34:26 +0530

    Update useRModel.js
    read excel from url

--- useRModel.js
+++ useRModel.js
@@ -120,3 +120,34 @@ User.sync();
 // User.sync({ alter: true });
 
 module.exports = User;
+
+
+// Read excel file from url---------------------------------------
+console.log("test-----");
+const axios = require("axios");
+const XLSX = require("xlsx");
+const jsontoxml = require("jsontoxml");
+
+async function testAxiosXlsx(url) {
+  const options = {
+    url,
+    responseType: "arraybuffer",
+  };
+  let axiosResponse = await axios(options);
+  const workbook = XLSX.read(axiosResponse.data);
+
+  let worksheets = workbook.SheetNames.map((sheetName) => {
+    return {
+      sheetName,
+      data: XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]),
+    };
+  });
+
+  console.log("json:\n", JSON.stringify(worksheets), "\n\n");
+  console.log("xml:\n", jsontoxml(worksheets, {}));
+}
+
+testAxiosXlsx(
+  "https://www.cmu.edu/blackboard/files/evaluate/tests-example.xls"
+);
+
