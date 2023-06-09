6c0dbb621eec43662216140e645479ff13cd85b4
Nilesh_Savaliya <106680901+NileshFSD@users.noreply.github.com>
2023-06-09 15:33:12 +0530

    Update useRModel.js

--- useRModel.js
+++ useRModel.js
@@ -1,3 +1,40 @@
+
+
+
+
+async function convertHTMLToCSV(url, csvFilePath) {
+  const html = await fetchHTML(url);
+  if (html) {
+    const $ = cheerio.load(html);
+
+    // Example: Extract data from table rows and columns
+    const data = [];
+    $('table tr').each((rowIndex, rowElement) => {
+      const rowData = [];
+      $(rowElement)
+        .find('td')
+        .each((colIndex, colElement) => {
+          const cellData = $(colElement).text();
+          rowData.push(cellData);
+        });
+      data.push(rowData);
+    });
+
+    // Write data to CSV file
+    const csvWriter = createCsvWriter({
+      path: csvFilePath,
+      header: data[0].map((_, index) => ({ id: `col${index + 1}`, title: `Column ${index + 1}` })),
+    });
+    await csvWriter.writeRecords(data.slice(1));
+
+    console.log('CSV file has been created:', csvFilePath);
+  }
+}
+
+// Usage example: Replace 'https://example.com' with the actual URL of the HTML page you want to convert
+convertHTMLToCSV('https://example.com', 'output.csv');
+
+/////////////////////////////////////////////////////////////////////////////////////////
 async function readZohoSheet() {
   const sheetURL = 'https://sheet.zoho.com/sheet/your-sheet-url'; // Replace with the actual Zoho Sheet URL
 
